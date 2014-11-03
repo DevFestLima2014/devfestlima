bfafadd793681fd215c9dbd4d335eba4e41e3883
daulys <daulys2@gmail.com>
2014-11-03 10:18:34 -0500

    restful1
    testing

---
+++ my-rest-api/models/Robots.php
@@ -0,0 +1,60 @@
+<?php
+
+use Phalcon\Mvc\Model,
+    Phalcon\Mvc\Model\Message,
+    Phalcon\Mvc\Model\Validator\InclusionIn,
+    Phalcon\Mvc\Model\Validator\Uniqueness;
+
+class Robots extends Model
+{
+
+    public function validation()
+    {
+        //Type must be: droid, mechanical or virtual
+        $this->validate(new InclusionIn(
+            array(
+                "field"  => "type",
+                "domain" => array("droid", "mechanical", "virtual")
+            )
+        ));
+
+        //Robot name must be unique
+        $this->validate(new Uniqueness(
+            array(
+                "field"   => "name",
+                "message" => "The robot name must be unique"
+            )
+        ));
+
+        //Year cannot be less than zero
+        if ($this->year < 0) {
+            $this->appendMessage(new Message("The year cannot be less than zero"));
+        }
+
+        //Check if any messages have been produced
+        if ($this->validationHasFailed() == true) {
+            return false;
+        }
+    }
+
+}
+
+
+$di = new \Phalcon\DI\FactoryDefault();
+
+//Set up the database service
+$di->set('db', function(){
+    return new \Phalcon\Db\Adapter\Pdo\Mysql(array(
+        "host" => "localhost",
+        "username" => "root",
+        "password" => "rootbiomedical",
+        "dbname" => "robotics"
+    ));
+});
+
+$app = new \Phalcon\Mvc\Micro();
+
+//Bind the DI to the application
+$app->setDI($di);
+
+
