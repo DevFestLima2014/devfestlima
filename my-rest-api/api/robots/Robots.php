f8efa2b9f71e9a0a7e37571493d0ea84499dee63
daulys <daulys2@gmail.com>
2014-11-03 11:07:49 -0500

    rest
    rest

--- my-rest-api/api/robots/Robots.php
+++ my-rest-api/api/robots/Robots.php
@@ -58,3 +58,176 @@ $app = new \Phalcon\Mvc\Micro();
 $app->setDI($di);
 
 
+
+//Retrieves all robots
+$app->get('/api/robots', function() use ($app) {
+
+    $phql = "SELECT * FROM Robots ORDER BY name";
+    $robots = $app->modelsManager->executeQuery($phql);
+
+    $data = array();
+    foreach ($robots as $robot) {
+        $data[] = array(
+            'id' => $robot->id,
+            'name' => $robot->name,
+        );
+    }
+
+    echo json_encode($data);
+});
+
+
+
+//Searches for robots with $name in their name
+$app->get('/api/robots/search/{name}', function($name) use ($app) {
+
+    $phql = "SELECT * FROM Robots WHERE name LIKE :name: ORDER BY name";
+    $robots = $app->modelsManager->executeQuery($phql, array(
+        'name' => '%' . $name . '%'
+    ));
+
+    $data = array();
+    foreach ($robots as $robot) {
+        $data[] = array(
+            'id' => $robot->id,
+            'name' => $robot->name,
+        );
+    }
+
+    echo json_encode($data);
+
+});
+
+
+//Retrieves robots based on primary key
+$app->get('/api/robots/{id:[0-9]+}', function($id) use ($app) {
+
+    $phql = "SELECT * FROM Robots WHERE id = :id:";
+    $robot = $app->modelsManager->executeQuery($phql, array(
+        'id' => $id
+    ))->getFirst();
+
+    if ($robot == false) {
+        $response = array('status' => 'NOT-FOUND');
+    } else {
+        $response = array(
+            'status' => 'FOUND',
+            'data' => array(
+                'id' => $robot->id,
+                'name' => $robot->name
+            )
+        );
+    }
+
+    echo json_encode($response);
+});
+
+//Adds a new robot
+$app->post('/api/robots', function() use ($app) {
+
+    $robot = json_decode($app->request->getRawBody());
+
+    $phql = "INSERT INTO Robots (name, type, year) VALUES (:name:, :type:, :year:)";
+
+    $status = $app->modelsManager->executeQuery($phql, array(
+        'name' => $robot->name,
+        'type' => $robot->type,
+        'year' => $robot->year
+    ));
+
+    //Check if the insertion was successful
+    if ($status->success() == true) {
+
+        $this->response->setStatusCode(201, "Created")->sendHeaders();
+
+        $robot->id = $status->getModel()->id;
+
+        $response = array('status' => 'OK', 'data' => $robot);
+
+    } else {
+
+        //Change the HTTP status
+        $this->response->setStatusCode(409, "Conflict")->sendHeaders();
+
+        //Send errors to the client
+        $errors = array();
+        foreach ($status->getMessages() as $message) {
+            $errors[] = $message->getMessage();
+        }
+
+        $response = array('status' => 'ERROR', 'messages' => $errors);
+
+    }
+
+    echo json_encode($response);
+
+});
+
+//Updates robots based on primary key
+$app->put('/api/robots/{id:[0-9]+}', function($id) use($app) {
+
+    $robot = json_decode($app->request->getRawBody());
+
+    $phql = "UPDATE Robots SET name = :name:, type = :type:, year = :year: WHERE id = :id:";
+    $status = $app->modelsManager->executeQuery($phql, array(
+        'id' => $id,
+        'name' => $robot->name,
+        'type' => $robot->type,
+        'year' => $robot->year
+    ));
+
+    //Check if the insertion was successful
+    if ($status->success() == true) {
+
+        $response = array('status' => 'OK');
+
+    } else {
+
+        //Change the HTTP status
+        $this->response->setStatusCode(409, "Conflict")->sendHeaders();
+
+        $errors = array();
+        foreach ($status->getMessages() as $message) {
+            $errors[] = $message->getMessage();
+        }
+
+        $response = array('status' => 'ERROR', 'messages' => $errors);
+
+    }
+
+    echo json_encode($response);
+
+});
+
+//Deletes robots based on primary key
+$app->delete('/api/robots/{id:[0-9]+}', function($id) use ($app) {
+
+    $phql = "DELETE FROM Robots WHERE id = :id:";
+    $status = $app->modelsManager->executeQuery($phql, array(
+        'id' => $id
+    ));
+    if ($status->success() == true) {
+
+        $response = array('status' => 'OK');
+
+    } else {
+
+        //Change the HTTP status
+        $this->response->setStatusCode(409, "Conflict")->sendHeaders();
+
+        $errors = array();
+        foreach ($status->getMessages() as $message) {
+            $errors[] = $message->getMessage();
+        }
+
+        $response = array('status' => 'ERROR', 'messages' => $errors);
+
+    }
+
+    echo json_encode($response);
+
+});
+
+?>
+
+
